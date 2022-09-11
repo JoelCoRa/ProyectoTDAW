925651eeed52525469601020f554a0fb62b2d0e8
Joel Colin <joelcolinr@gmail.com>
2022-09-10 22:29:59 -0500

    Fin dia 5!

---
+++ validar.php
@@ -0,0 +1,23 @@
+<?php
+
+    
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $sexo = $_POST['sexo'];
+    $correo = $_POST['correo'];
+    $telefono = $_POST['telefono'];
+    ?>
+    <div class="pagina-respuesta">
+    <?php 
+        echo "<p class='pagina-respuesta'>  $nombre </p>";
+    ?>
+    </div>
+    
+<?php
+
+
+
+
+
+
+    
